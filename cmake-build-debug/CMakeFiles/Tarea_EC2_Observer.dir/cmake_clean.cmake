b9c0497447cedb6d7a513e22b92b4fc0c86badb8
MAU143429 <mau141226@estudiantec.cr>
2021-03-23 14:23:12 -0600

    Project Created

---
+++ cmake-build-debug/CMakeFiles/Tarea_EC2_Observer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea_EC2_Observer.dir/main.cpp.o"
+  "Tarea_EC2_Observer"
+  "Tarea_EC2_Observer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea_EC2_Observer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
